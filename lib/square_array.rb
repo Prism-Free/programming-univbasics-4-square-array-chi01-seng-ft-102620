7bb7739129536c32cb9442270c1959bafce7c11e
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-10 21:04:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   counter = 0
   new_array = []
   
-  while array[counter] do
-   new_array<<(array[counter] * (array[counter])
+  while array[counter] {
+   new_array<<(array[counter] * array[counter])
    counter += 1
-  end
+  }
   new_array
 end
